3ddbfd80636312f2e29de3e22d4173453982f3ae
MRSlothAlan <42954802+MRSlothAlan@users.noreply.github.com>
2019-04-27 22:16:40 +0800

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,125 @@
+var container_div = document.getElementById('intro');
+var inner_div = document.getElementById('inner');
+var mask1 = document.getElementById("mask1");
+var mask2 = document.getElementById("mask2");
+
+//setting mouse object
+var mouse = {
+  //properties
+  _x: 0, _y: 0,
+  x: 0, y: 0,
+  //methods
+  updatePosition: function(event){
+    var e = event || window.event; //receive event
+    this.x = e.clientX - this._x;
+    this.y = (e.clientY - this._y)*-1;
+    //this.y need to be reversed since upper part of the y-axis is
+    //positive but after upper minus center it becomes negative
+  },
+  setOrigin: function(e){//finding the origin/center of the element
+    this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+    this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+  },
+  show: function() {return '(' + this.x + ',' + this.y + ')';}
+}
+//---------------------------------------------------------------------------------
+
+
+mouse.setOrigin(container_div);
+
+var counter = 0;
+var updateRate = 10;
+//return a boolean about whether it is time to update. set the rate of update
+var isTimeToUpdate = function(){
+  return (counter++ % updateRate === 0);
+}
+//update the rotation of the inner div element
+var updateTransformStyle = function(x,y){
+  var style = "rotateX("+x+"deg) rotateY("+y+"deg)";
+  inner.style.transform = style;
+  inner.style.webkitTransform = style;
+  inner.style.mozTransform = style;
+  inner.style.msTransform = style;
+  inner.style.oTransform = style;
+}
+
+var update = function(event){
+  mouse.updatePosition(event);
+  updateTransformStyle(
+    //Convert a number into a string, keeping only two decimals:
+    (mouse.y/inner.offsetHeight/2).toFixed(2),
+    (mouse.x/inner.offsetWidth/2).toFixed(2)
+  );
+};
+
+var onMouseEnterHandler = function(event){ //set up several handler functions for receiving events
+  update(event);
+};
+var onMouseLeaveHandler = function(){
+  inner.style = "";
+};
+var onMouseMoveHandler = function(event){
+  if(isTimeToUpdate()){ // means if the function returns true
+    update(event);
+  }
+};
+
+
+container_div.onmouseenter = onMouseEnterHandler;
+container_div.onmouseleave = onMouseLeaveHandler;
+container_div.onmousemove = onMouseMoveHandler;
+
+
+var A_CT1_div = document.getElementById("content_text_one");
+var A_P1_div = document.getElementById("one_photo");
+var A_CT2_div = document.getElementById("content_text_two");
+var A_P2_div = document.getElementById("two_photo");
+
+function ADD_styles_one(){
+  A_CT1_div.classList.add("up_class");
+  A_P1_div.classList.add("up_class");
+
+  A_CT1_div.classList.remove("off_class");
+  A_P1_div.classList.remove("off_class");
+
+  A_CT1_div.style.opacity = "1";
+  A_P1_div.style.opacity = "1";
+  A_CT2_div.style.opacity = "1";
+  A_P2_div.style.opacity = "1";
+}
+
+function REMOVE_styles_one(){
+  A_CT1_div.classList.remove("up_class");
+  A_P1_div.classList.remove("up_class");
+
+  A_CT1_div.classList.add("off_class");
+  A_P1_div.classList.add("off_class");
+
+  A_CT1_div.style.opacity = "0";
+  A_P1_div.style.opacity = "0";
+  A_CT2_div.style.opacity = "0";
+  A_P2_div.style.opacity = "0";
+}
+
+function Add_the_suitable_wheel_event(){
+  var ABOUT_page_body = document.body;
+  if(ABOUT_page_body.addEventListener){
+    ABOUT_page_body.addEventListener("mousewheel", Check_wheel, false);
+    ABOUT_page_body.addEventListener("DOMMouseScroll", Check_wheel, false);
+  }
+  else{
+    ABOUT_page_body.attachEvent("onmousewheel", Check_wheel);
+  }
+}
+
+function Check_wheel(){ //can be used throughtout the webpage for adding new style
+  var e = window.event || event;
+  var delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;
+  var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  if((scrollTop >= 1300) && (delta == 1)){ //downward
+    ADD_styles_one();
+  }
+  else if((scrollTop <= 900) && (delta == -1)){
+    REMOVE_styles_one();
+  }
+}
---
+++ JS/main.js
@@ -0,0 +1,63 @@
+function hide_menu(){
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+
+var Upper_menu_ul = document.getElementById("upper_menu_bar");
+var Toggle_div = document.getElementById("toggle_menu");
+
+var Item_a = Upper_menu_ul.getElementsByTagName("a");
+var Stroke_div = Toggle_div.getElementsByTagName("div");
+
+let len = Item_a.length;
+let len_stk = Stroke_div.length;
+i=0;
+j=0;
+
+  if (prevScrollpos > currentScrollPos) {
+    Toggle_div.style.top = "5px" ;
+    Upper_menu_ul.style.top = "0px";
+  } else {
+    Toggle_div.style.top = "-100px";
+    Upper_menu_ul.style.top = "-100px";
+  }
+
+  if (currentScrollPos > 700){
+    for (i; i < len; i++) {
+       Item_a[i].style.color = "black";
+    }
+    for (j; j < len_stk; j++){
+      Stroke_div[j].style.backgroundColor = "black";
+    }
+    Stroke_div[1].style.backgroundColor = "grey";
+  }
+  else{
+    for (i; i < len; i++) {
+       Item_a[i].style.color = "white";
+    }
+    for (j; j < len_stk;j++){
+      Stroke_div[j].style.backgroundColor = "white";
+    }
+    Stroke_div[1].style.backgroundColor = "grey";
+  }
+
+  prevScrollpos = currentScrollPos;
+}
+
+}
+function toggle_menu(){
+  document.getElementById("menu_content").classList.toggle('change_menu');
+}
+
+function animated_menu(){
+  document.getElementById("SK1").classList.toggle('change_1');
+  document.getElementById("SK2").classList.toggle('change_2');
+  document.getElementById("SK3").classList.toggle('change_3');
+  document.getElementById("toggle_menu").classList.toggle('change_toggle');
+}
+function START(){
+  var Preloader_div = document.getElementById("preloader");
+  var Intro_div = document.getElementById("intro_pic");
+  Preloader_div.style.left = "-100%";
+  Preloader_div.style.top = "-100%";
+}
